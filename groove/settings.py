824b23681e7778be19f8bcae73433bccc612b7fe
AmoghSaxena <13078900+AmoghSaxena@users.noreply.github.com>
2024-07-15 16:01:45 +0100

    updated docker-compose.yml

--- groove/settings.py
+++ groove/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qbd%izz@w@e8jzl9(nn%ow5s^m5@wv+!l1qysp=1)tcm+g6d7%
 
 # SECURITY WARNING: don't run with debug turned on in production!
 prod_DEBUG = os.getenv('DEBUG_MODE', 'True')
-if prod_DEBUG == 'False':
+if prod_DEBUG == 'false':
     DEBUG = False
 else:
     DEBUG = True
